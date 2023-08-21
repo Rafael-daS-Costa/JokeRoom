601f4bb606b06728085d1d941ada6c3515cd6b8e
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2023-08-21 11:27:09 -0300

    Novas piadas e possibilidade de escolha

--- src/entities/JokeList.java
+++ src/entities/JokeList.java
@@ -4,20 +4,18 @@ import java.net.*;
 public class JokeList {
 	private static final int CHOOSING = 0;
 	private static final int SENTJOKE = 1;
-	private static final int ANOTHER = 2;
-	private static final int SENTCLUE = 3;
+	private static final int SENTCLUE = 2;
+	private static final int ANOTHER = 3;
 	
 	private int state = CHOOSING;
 	private int currentJoke = 0;
 	
-	private String[] toctoc = { "Noé", "Dem", "Mário", "KGB", "Who" };
+	private String[] toctoc = { "Noé", "Dem", "Mário", "KGB"};
     private String[] respostas = { "Noé da sua conta.",
                                  "Demorou pra responder e eu esqueci a piada.",
                                  "Deixa quieto.",
-                                 "Slap! Nós quem fazemos as perguntas!",
-                                 "Is there an echo in here?" };
-    
-    private String[] historinha = {
+                                 "Slap! Nós quem fazemos as perguntas!"};
+	private String[] historinha = {
 					    		"Seu delegado, meu marido saiu de casa hoje"
 					    		+" cedo pra comprar arroz e não voltou, o que faço?",
 					    		"Contratei um personal que me recomendou uma série"
@@ -57,4 +55,48 @@ public class JokeList {
 								"Se tirar o anel leva metade da casa.",
 								"H Deus O."
 								};
+    
+    public String processInput(String theInput) {
+    	String theOutput = null;
+    	
+    	if(state == CHOOSING) {
+    		theOutput = "Qual o tipo de piada você deseja?" + 
+    				"[A]divinhas, [p]ontinho, [h]istorinha, ou [t]oc toc?";
+    		state = SENTJOKE;
+    	} else if(state == SENTJOKE) {
+    		// checa se a escolha de piadas foi feita corretamente
+    		if(theInput.equalsIgnoreCase("a") ||
+    				theInput.equalsIgnoreCase("p") ||
+    				theInput.equalsIgnoreCase("h") ||
+    				theInput.equalsIgnoreCase("t")) {
+    			// começa o fluxo da piada escolhida
+    			if(theInput.equalsIgnoreCase("a")) {
+    				theOutput = adivinhas[currentJoke];
+    				state = SENTCLUE;
+    			}
+    			if(theInput.equalsIgnoreCase("h")) {
+    				theOutput = historinha[currentJoke];
+    				state = SENTCLUE;
+    			}
+    			if(theInput.equalsIgnoreCase("p")) {
+    				theOutput = pontinho[currentJoke];
+    				state = SENTCLUE;
+    			}
+    			if(theInput.equalsIgnoreCase("t")) {
+    				theOutput = toctoc[currentJoke];
+    				state = SENTCLUE;
+    			}
+    			
+    		}else {
+    			theOutput = "Escolha inválida" +
+    					"Escolha um tipo de piada dentre as possíveis!" +
+    					"Qual o tipo de piada você deseja?" +
+    					"[A]divinhas, [p]ontinho, [h]istorinha, ou [t]oc toc?";
+    		}
+    	} else if (state == SENTCLUE) {
+    		
+    	}
+    	
+    	return "";
+    }
 }
